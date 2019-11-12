32c626e65b08194bb5f7dcf671d057d4639cb3e9
Warren Gates <warrenckhan@gmail.com>
2019-11-11 23:36:31 -0500

    movingaxios post to getinitialprops

--- src/api/github/index.js
+++ src/api/github/index.js
@@ -1,64 +1,20 @@
+const cacheableResponse = require('cacheable-response');
 const axios = require('axios');
-const ssrCache = require('../../ssrCache');
 module.exports = (app, server) => {
-	server.get('/github', (req, res) => {
-		var config = {
-			headers: { Authorization: 'bearer ' + process.env.GITHUB_TOKEN }
-		};
-		const q = `{
-  viewer {
-    login
-    avatarUrl
-    bio
-    isHireable
-    isBountyHunter
-    organizations(first: 3) {
-      edges {
-        node {
-          name
-          description
-        }
-      }
-    }
-    starredRepositories(last: 3) {
-      edges {
-        node {
-          name
-          description
-        }
-      }
-    }
-    watching(last: 3) {
-      edges {
-        node {
-          name
-          description
-        }
-      }
-    }
-  }
-}
+  const ssrCache = cacheableResponse({
+    ttl: 250 * 60 * 60, // 1hour
+    get: async ({ req, res, pagePath, queryParams }) => ({
+      data: await app.renderToHTML(req, res, pagePath, queryParams)
+    }),
+    send: ({ data, res }) => res.send(data)
+  });
 
-`;
-		const v = `{
-    }
-		`;
-		let resp;
-		axios
-			.post(
-				'https://api.github.com/graphql',
-				{ query: q, variables: v },
-				config
-			)
-			.then(resp => {
-				const queryParams = {
-					githubData: resp.data.data.viewer
-				};
-				return ssrCache({ req, res, pagePath: '/github', queryParams });
-			})
-			.catch(err => {
-				console.log('Error:', err);
-				return ssrCache({ req, res, pagePath: '/github' });
-			});
+
+  /**
+   * Gets Github Data Using API
+   */
+	server.get('/github', (req, res) => {
+
+      return ssrCache({ req, res, pagePath: '/github' });
 	});
 };
--- src/core.js
+++ src/core.js
@@ -3,6 +3,14 @@ const port = parseInt(process.env.PORT, 10) || 8989;
 
 module.exports = function core(app, server) {
 	if(process.env.NODE_ENV !=='backend'){
+		const cacheableResponse = require('cacheable-response');
+		const ssrCache = cacheableResponse({
+			ttl: 250 * 60 * 60, // 1hour
+			get: async ({ req, res, pagePath, queryParams }) => ({
+				data: await app.renderToHTML(req, res, pagePath, queryParams)
+			}),
+			send: ({ data, res }) => res.send(data)
+		});
 		const handle = app.getRequestHandler();
 		server.get('/', (req, res) => {
 			return ssrCache({req, res, pagePath:'/'});
